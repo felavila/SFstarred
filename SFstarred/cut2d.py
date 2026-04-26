38711a48f18aed3b81f2ee8e942f715271ccb0f0
felavila <felipeavila41@gmail.com>
2026-04-26 17:42:37 -0400

    .

--- SFstarred/cut2d.py
+++ SFstarred/cut2d.py
@@ -30,6 +30,7 @@ def find_nearest_object(array, coord):
         metric = np.sqrt((array[:, 0]-coord_rep[:, 0])**2 + (array[:, 1]-coord_rep[:, 1])**2)
         idx = np.argmin(metric, axis=0)
         return array[idx]
+    
 def create_rectangle_patch(data,center, size=1.0):
     x, y = center
     width = height = size
